d621475f624faa307926f59373fc531231ec8c8d
Gina <g26delarosa@gmail.com>
2018-09-10 09:31:34 -0700

    Added charts vc

---
+++ ClerkieiOSChallenge/Controller/ChartsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ChartsViewController.swift
+//  Berkie
+//
+//  Created by Gina De La Rosa on 9/10/18.
+//  Copyright © 2018 Gina De La Rosa. All rights reserved.
+//
+
+import UIKit
+
+class ChartsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
